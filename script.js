09aa861503d98891876ab6412b5515fd9b86aef8
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2023-12-04 15:58:18 +0500

    news

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-let list = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let dots = document.querySelectorAll('.slider .dots li');
-let previous = document.getElementById('previous');
-let next = document.getElementById('next');
+let list = document.querySelector(".slider .list");
+let items = document.querySelectorAll(".slider .list .item");
+let dots = document.querySelectorAll(".slider .dots li");
+let previous = document.getElementById("previous");
+let next = document.getElementById("next");
 let active = 0;
 let lengthItems = items.length - 1;
 next.onclick = function () {
-    if (active + 1 > lengthItems) {
-        active = 0;
-    } else {
-        active = active + 1;
-    }
-    reloadSlider();
-}
-previous.onclick = function() {
-    if (active - 1 < 0) {
-        active = lengthItems;
-    } else {
-        active = active - 1;
-    }
-    reloadSlider()
-}
+  if (active + 1 > lengthItems) {
+    active = 0;
+  } else {
+    active = active + 1;
+  }
+  reloadSlider();
+};
+previous.onclick = function () {
+  if (active - 1 < 0) {
+    active = lengthItems;
+  } else {
+    active = active - 1;
+  }
+  reloadSlider();
+};
 let refresh = setInterval(() => {
-    next.click()
+  next.click();
 }, 3000);
 function reloadSlider() {
-    let checkleft = items[active].offsetLeft;
-    list.style.left = -checkleft + 'px';
+  let checkleft = items[active].offsetLeft;
+  list.style.left = -checkleft + "px";
 
-    let lastActiveDot = document.querySelector('.slider .dots li.active');
-    lastActiveDot.classList.remove('active');
-    dots[active].classList.add('active');
-    clearInterval(refresh);
-    refresh = setInterval(() => {
-        next.click()
-    }, 3000);
+  let lastActiveDot = document.querySelector(".slider .dots li.active");
+  lastActiveDot.classList.remove("active");
+  dots[active].classList.add("active");
+  clearInterval(refresh);
+  refresh = setInterval(() => {
+    next.click();
+  }, 3000);
 }
 
 dots.forEach((li, key) => {
-    li.addEventListener('click',
-        function () {
-            active = key;
-            reloadSlider();
-    })
-})
+  li.addEventListener("click", function () {
+    active = key;
+    reloadSlider();
+  });
+});
